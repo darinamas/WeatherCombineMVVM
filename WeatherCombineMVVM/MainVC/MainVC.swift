9ac693412d2886fd70f59e946db3a9af183b5c2b
Darinka <dpolevyk@post.cz>
2021-11-01 19:44:54 +0100

    added  new new publishers and subscriptions.

--- WeatherCombineMVVM/MainVC/MainVC.swift
+++ WeatherCombineMVVM/MainVC/MainVC.swift
@@ -13,11 +13,55 @@ class MainVC: UIViewController {
     
     @IBOutlet weak var wordLabel: UILabel!
     @IBOutlet weak var tempLabel: UILabel!
+
+    @IBOutlet weak var switcher: UISwitch!
+    
+    @IBOutlet weak var greyButton: UIButton!
+    @IBOutlet weak var redButton: UIButton!
+    
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var labelForButton: UILabel!
+    
+    private var switchSubscribers = Set<AnyCancellable>() //subscriber
     
     var viewModel = ViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+      //  viewModel.switcherChanged(button1: greyButton, button2: redButton, button3: greenButton)
+        
+        // Create a publisher
+        let publisher = NotificationCenter.Publisher(center: .default, name: .titleNotification, object: nil)
+                                        .map { (notification) -> String? in
+             return (notification.object as? titleStruct)?.title ?? ""
+        }
+        
+        let publisher2 = NotificationCenter.Publisher(center: .default, name: .titleNotification, object: nil)
+                                        .map { (notification) -> UIColor? in
+                                    return (notification.object as? titleStruct)?.color
+        }
+        
+        // Create a subscriber
+        let subscriber = Subscribers.Assign(object: labelForButton, keyPath: \.text)
+        publisher.subscribe(subscriber)
+        
+        // Create a subscriber
+        let subscriber2 = Subscribers.Assign(object: labelForButton, keyPath: \.backgroundColor)
+        publisher2.subscribe(subscriber2)
+        
+        //create subscribers
+        switchSubscribers = [
+            viewModel.$canChangeButton
+                .receive(on: DispatchQueue.main)
+                .assign(to: \.isEnabled, on: redButton),
+            viewModel.$canChangeButton
+                .receive(on: DispatchQueue.main)
+                .assign(to: \.isEnabled, on: greenButton),
+            viewModel.$canChangeButton
+                .receive(on: DispatchQueue.main)
+                .assign(to: \.isEnabled, on: greyButton)
+        ]
+       
     }
 
     @IBAction func buttonAction(_ sender: Any) {
@@ -32,6 +76,44 @@ class MainVC: UIViewController {
             .store(in: &anyCancelable)
     }
     
-
+    //Switcher
+    @IBAction func switchAction(_ sender: UISwitch) {
+        viewModel.canChangeButton = sender.isOn
+    }
     
+    @IBAction func redButtonTapped(_ sender: Any) {
+        viewModel.setOutputs(button: "Red")
+        viewModel.setOutputsColor(button: "Red")
+        postNotification()
+    }
+    
+    @IBAction func greyButtonTapped(_ sender: Any) {
+        viewModel.setOutputs(button: "Grey")
+        viewModel.setOutputsColor(button: "Grey")
+        postNotification()
+    }
+    
+    
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        viewModel.setOutputs(button: "Green")
+        viewModel.setOutputsColor(button: "Green")
+        postNotification()
+    }
+    
+    func postNotification() {
+         // We can fire the notification when the user taps the button.
+         // Post the notification
+        let title = viewModel.textForLabel ?? "Coming soon"
+        let color = viewModel.textColor
+        let newTitle = titleStruct(title: title, color: color!)
+        NotificationCenter.default.post(name: .titleNotification, object: newTitle)
+    }
+}
+
+extension Notification.Name {
+    static let titleNotification = Notification.Name("newTitle")
+}
+struct titleStruct {
+    let title: String
+    let color: UIColor
 }
--- WeatherCombineMVVM/MainVC/ViewModel.swift
+++ WeatherCombineMVVM/MainVC/ViewModel.swift
@@ -7,11 +7,20 @@
 
 import Foundation
 import Combine
+import UIKit
 
 class ViewModel {
     
-    @Published var data = [""]
-    private var anyCancelable = Set<AnyCancellable>()
+    @Published var data = [""]  //publisher
+    private var anyCancelable = Set<AnyCancellable>() //subscriber
+    
+    @Published var canChangeButton: Bool = false  //publisher
+    
+    var textForLabel: String = ""
+    var textColor: UIColor?
+//    private var switchSubscriber1: AnyCancellable? //subscriber1 fro grey button
+//    private var switchSubscriber2: AnyCancellable? //subscriber2 fro red button
+//    private var switchSubscriber3: AnyCancellable? //subscriber3 fro red button
     
     func fetch() {
         NetworkManager.shared.getResults()
@@ -39,5 +48,44 @@ class ViewModel {
         return array.randomElement() ?? "🦀"
         
     }
+    
+    func setOutputs(button: String) {
+        switch button {
+        case "Grey": textForLabel = "Grey button tapped"
+        case "Red": textForLabel = "Red button tapped"
+        case "Green": textForLabel = "Green button tapped"
+        default: textForLabel = "Coming soon"
+        }
+        
+    }
+    
+    func setOutputsColor(button: String) {
+        switch button {
+        case "Grey": textColor = UIColor.gray
+        case "Red": textColor = UIColor.red
+        case "Green": textColor = UIColor.green
+        default: textColor = UIColor.blue
+        }
+        
+    }
+//
+//    func switcherChanged(button1: UIButton, button2: UIButton, button3: UIButton) {
+//        // Save the cancellable subscription
+//        switchSubscriber1 = $canChangeButton
+//             .receive(on: DispatchQueue.main)
+//             .assign(to: \.isEnabled, on: button1)
+//        
+//        switchSubscriber2 = $canChangeButton
+//             .receive(on: DispatchQueue.main)
+//             .assign(to: \.isEnabled, on: button2)
+//        
+//        switchSubscriber3 = $canChangeButton
+//             .receive(on: DispatchQueue.main)
+//             .assign(to: \.isEnabled, on: button3)
+//        
+//    }
+    
+    
+    
     init() {    }
 }
